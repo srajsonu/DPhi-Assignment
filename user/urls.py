23de6848b42edc16683920ac046be8928fe70783
SONU RAJ <srajsonu02@gmail.com>
2020-12-20 16:14:36 +0530

    minor changes

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ app_name = 'user'
 urlpatterns = [
     path('', views.signin, name='signin'),
 
-    path('index/', views.index, name='signup_index'),
+    path('index/', views.index, name='index'),
     path('signup/<int:user>/', views.signup_type, name='signup_type'),
     path('signup/post/<int:user>/', views.signup, name='signup'),
 
